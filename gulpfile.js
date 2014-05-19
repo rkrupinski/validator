1387223d26948bddf13495bbee705266db68865f
Rafał Krupiński <krupinski.rafal@gmail.com>
2014-05-19 19:34:43 +0200

    Updated build & deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,8 @@
 var gulp = require('gulp')
-  , browserify = require('gulp-browserify')
   , jshint = require('gulp-jshint');
 
 gulp.task('lint', function () {
   gulp.src('./lib/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('bundle', function () {
-  gulp.src('./lib/index.js')
-    .pipe(browserify({
-      standalone: 'validator'
-    }))
-    .pipe(gulp.dest('./build'));
-});
-
-gulp.task('dev', function () {
-  gulp.watch('./lib/**/*.js', [
-    'lint',
-    'bundle'
-  ]);
 });
