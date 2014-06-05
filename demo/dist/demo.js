0859adb96849f2268d0f80f3c84dce941e9b533d
Rafał Krupiński <krupinski.rafal@gmail.com>
2014-06-05 19:20:57 +0200

    Demo fix

--- demo/dist/demo.js
+++ demo/dist/demo.js
@@ -5,8 +5,10 @@ var validator = require('../../lib/index');
 
 validator(document.querySelector('.js_form'), {
   submitHandler: function (form) {
-    this.validate() && alert('Yay!');
-    form.reset();
+    if (this.validate()) {
+      alert('Yay!');
+      form.reset();
+    }
   }
 });
 
--- demo/src/demo.js
+++ demo/src/demo.js
@@ -4,7 +4,9 @@ var validator = require('../../lib/index');
 
 validator(document.querySelector('.js_form'), {
   submitHandler: function (form) {
-    this.validate() && alert('Yay!');
-    form.reset();
+    if (this.validate()) {
+      alert('Yay!');
+      form.reset();
+    }
   }
 });
